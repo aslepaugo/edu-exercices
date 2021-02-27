4822ce452148a15f8c14da194c83b13c26f06ddf
Aliaksei Slepau <aslepaugo@gmail.com>
2021-02-27 08:56:22 +0100

    day3 exercises

---
+++ day3/exercises.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    int x = 150;
+    float f = 15.933f;
+    short y = 250;
+
+    std::cout<<"x = "<< x <<std::endl;
+    std::cout<<"f = "<< f <<std::endl;
+    std::cout<<"y = "<< y <<std::endl;
+
+    short day = 1;
+    std::string month = "January";
+    short year = 1980;
+
+    std::cout<<"My birthday date: "<< day <<" "<< month << " " << year << std::endl;
+}
